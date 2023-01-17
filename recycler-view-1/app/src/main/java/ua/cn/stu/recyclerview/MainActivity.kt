7c512688caed4d6f3c058ce1143130fa6972f8e7
MichaelZolotov77 <michaelzol@yandex.ru>
2023-01-17 16:36:31 +0300

    DiffUtil

--- recycler-view-1/app/src/main/java/ua/cn/stu/recyclerview/MainActivity.kt
+++ recycler-view-1/app/src/main/java/ua/cn/stu/recyclerview/MainActivity.kt
@@ -3,6 +3,7 @@ package ua.cn.stu.recyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import ua.cn.stu.recyclerview.databinding.ActivityMainBinding
 import ua.cn.stu.recyclerview.model.User
@@ -22,9 +23,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val layoutManager = LinearLayoutManager(this)
         adapter = UsersAdapter(object : UserActionListener {
-            override fun onUserMove(user: User, moveBy: Int) {
+            override fun onUserMove(user: User, moveBy: Int, userPosition: Int) {
                 usersService.moveUser(user, moveBy)
+                // the workaround below retains the scroll position of the RecyclerView
+                // when user moves the top visible item
+                val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+                if (userPosition == firstVisibleItemPosition || (userPosition == firstVisibleItemPosition + 1 && moveBy < 0)) {
+                    val v = binding.recyclerView.getChildAt(0)
+                    val offset = if (v == null) 0 else v.top - binding.recyclerView.paddingTop
+                    layoutManager.scrollToPositionWithOffset(firstVisibleItemPosition, offset)
+                }
             }
 
             override fun onUserDelete(user: User) {
@@ -34,11 +44,19 @@ class MainActivity : AppCompatActivity() {
             override fun onUserDetails(user: User) {
                 Toast.makeText(this@MainActivity, "User: ${user.name}", Toast.LENGTH_SHORT).show()
             }
+
+            override fun onUserFire(user: User) {
+                usersService.fireUser(user)
+            }
         })
 
-        val layoutManager = LinearLayoutManager(this)
+
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = adapter
+        val itemAnimator = binding.recyclerView.itemAnimator
+        if (itemAnimator is DefaultItemAnimator) {
+            itemAnimator.supportsChangeAnimations = false
+        }
 
         usersService.addListener(usersListener)
     }
